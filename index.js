56bf772d2fdca5e0513539be5166aaf6ddd6684a
ihappysoni <happy.soni944@gmail.com>
2023-02-03 18:27:47 +0530

    attempt3

--- index.js
+++ index.js
@@ -21,6 +21,36 @@ app.use("/api",foodRouter)
 app.use("/api",homeRouter)
 
 
+const axios = require('axios');
+
+// Define your Github repository
+const OWNER = "ihappysoni";
+const REPO = "https://github.com/ihappysoni/BlogAppNode";
+
+// Define your personal access token
+const TOKEN = "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";
+
+// Build the API endpoint URL
+const url = `https://api.github.com/repos/${OWNER}/${REPO}/events`;
+
+// Send the GET request with your access token
+axios.get(url, {
+  headers: {
+    Authorization: `Token ${TOKEN}`
+  }
+})
+.then(response => {
+  // Parse the JSON response
+  const events = response.data;
+  // Do something with the event data
+})
+.catch(error => {
+  // Handle any errors that occurred
+  console.error("Request failed with error:", error);
+});
+
+
+
 app.listen(process.env.PORT||4000,()=>{
     console.log("app is  running");
 })
