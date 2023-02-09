1e7078b6149afa64a6cf603e01049115d9b435cb
ihappysoni <happy.soni944@gmail.com>
2023-02-09 14:55:26 +0530

    attempt 17

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # This is the back-end of Blog App of the web site given below
 @https://ihappysoni-blog-app-node.netlify.app/
+
+#to send the logs to new relic use the command
+node -r newrelic index.js
--- newrelic.js
+++ newrelic.js
@@ -1,42 +1,42 @@
-'use strict'
+'use strict';
 /**
- * New Relic agent configuration.
- *
- * See lib/config/default.js in the agent distribution for a more complete
- * description of configuration variables and their potential values.
- */
+* New Relic agent configuration.
+*
+* See lib/config/default.js in the agent distribution for a more complete
+* description of configuration variables and their potential values.
+*/
 exports.config = {
   /**
-   * Array of application names.
-   */
-  app_name: ['nodeAppBlog'],
+  * Array of application names.
+  */
+  app_name: ['BlogAppNode'],
   /**
-   * Your New Relic license key.
-   */
-  license_key: '5dc59a37501da129d314f363a52804fc17eeNRAL',
+  * Your New Relic license key.
+  */
+  license_key: '35c9f45b95eee8fe58f6e4aa8fd3180cFFFFNRAL',
   logging: {
     /**
-     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
-     * issues with the agent, 'info' and higher will impose the least overhead on
-     * production applications.
-     */
+    * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+    * issues with the agent, 'info' and higher will impose the least overhead on
+    * production applications.
+    */
     level: 'info'
   },
   /**
-   * When true, all request headers except for those listed in attributes.exclude
-   * will be captured for all traces, unless otherwise specified in a destination's
-   * attributes include/exclude lists.
-   */
+  * When true, all request headers except for those listed in attributes.exclude
+  * will be captured for all traces, unless otherwise specified in a destination's
+  * attributes include/exclude lists.
+  */
   allow_all_headers: true,
   attributes: {
     /**
-     * Prefix of attributes to exclude from all destinations. Allows * as wildcard
-     * at end.
-     *
-     * NOTE: If excluding headers, they must be in camelCase form to be filtered.
-     *
-     * @name NEW_RELIC_ATTRIBUTES_EXCLUDE
-     */
+    * Prefix of attributes to exclude from all destinations. Allows * as wildcard
+    * at end.
+    *
+    * NOTE: If excluding headers, they must be in camelCase form to be filtered.
+    *
+    * @env NEW_RELIC_ATTRIBUTES_EXCLUDE
+    */
     exclude: [
       'request.headers.cookie',
       'request.headers.authorization',
